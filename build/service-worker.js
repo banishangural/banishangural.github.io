993ac68bf193bfc373ac9220a80a3ff91a7283ce
Banish Angural <anguralbanish2@gmail.com>
2021-03-23 13:31:06 -0600

    New release without instagram

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da48f648b06c42be74bb13ef1762ee4f.js"
+  "/precache-manifest.f8882e80e34cf20942f720d27977e1f1.js"
 );
 
 self.addEventListener('message', (event) => {
